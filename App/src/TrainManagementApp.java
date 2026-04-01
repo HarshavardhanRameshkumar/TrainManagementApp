e494e34b412b8bf5d4598bf70467ea113817a407
harib@reiner <haribeeri1212@gmail.com>
2026-04-01 14:01:56 +0530

    UC2: Passenger Boarding

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -2,5 +2,7 @@ public class TrainManagementApp {
     public static void main(String[] args) {
         System.out.println("Train Management System Initialized.");
         System.out.println("Consist Summary: Engine, Passenger Car, Freight Car");
+        // UC2: Passengers boarding the Passenger Car
+        System.out.println("Passenger Car: 50 Passengers Boarded.");
     }
 }
