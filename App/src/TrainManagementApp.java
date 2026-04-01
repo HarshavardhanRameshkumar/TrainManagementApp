7894b6a858051d731d12ded31761eb624e8c846c
harib@reiner <haribeeri1212@gmail.com>
2026-04-01 14:04:57 +0530

    UC3: Load Freight Car

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -4,5 +4,7 @@ public class TrainManagementApp {
         System.out.println("Consist Summary: Engine, Passenger Car, Freight Car");
         // UC2: Passengers boarding the Passenger Car
         System.out.println("Passenger Car: 50 Passengers Boarded.");
+        // UC3: Load freight onto the Freight Car
+        System.out.println("Freight Car: Loading 5000kg of cargo.");
     }
 }
